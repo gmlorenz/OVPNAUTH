ef4f43a15cf897f6023aabe1b301acee63d3d85b
gmlorenz <35004437+gmlorenz@users.noreply.github.com>
2020-03-31 12:44:56 +0800

    updated serverconfigure.sh

--- serverconfigure.sh
+++ serverconfigure.sh
@@ -8,34 +8,28 @@
 
 
 clear
-echo "Welcome to JohnFordTV's Server Configure Script"
-echo "for Debian VPN"
+echo " Configurer PrivateconVPN"
+echo "Debian9,10 Ubuntu16 - x64"
 echo ""
 echo "Please type the Server Prefix"
-read -p "Prefix: " -e -i Prefix01 ServerPrefix
+read -p "Prefix: " -e -i PRIV01 ServerPrefix
 echo ""
 echo "Please type the Database IP"
-read -p "IP: " -e -i 127.0.0.1 DBhost
+read -p "IP: " -e -i 194.59.164.1 DBhost
 echo ""
 echo "Please type the Database Username"
-read -p "DB Username: " -e -i JohnFordTV DBuser
+read -p "DB Username: " -e -i u301507557_root DBuser
 echo ""
 echo "Please type the Database Password"
-read -p "DB Password: " -e -i JohnFordTV DBpass
+read -p "DB Password: " -e -i Privatecon DBpass
 echo ""
 echo "Please type the Database Name"
-read -p "DB Password: " -e -i JohnFordTV DBname
+read -p "DB Password: " -e -i u301507557_priv DBname
 echo ""
-echo "Okay, that's all I need. We are ready to setup your Panel now"
+echo "Okay, that's all I need. We are ready to setup your server now"
 read -n1 -r -p "Press any key to continue..."
 
 ## Configure Website
-sed -i "s/DBhost/$DBhost/g" ~/users.sh
-sed -i "s/DBuser/$DBuser/g" ~/users.sh
-sed -i "s/DBpass/$DBpass/g" ~/users.sh
-sed -i "s/DBname/$DBname/g" ~/users.sh
-sed -i "s/ServerPrefix/$ServerPrefix/g" ~/users.sh
-
 sed -i "s/DBhost/$DBhost/g" /etc/openvpn/login.sh
 sed -i "s/DBuser/$DBuser/g" /etc/openvpn/login.sh
 sed -i "s/DBpass/$DBpass/g" /etc/openvpn/login.sh
