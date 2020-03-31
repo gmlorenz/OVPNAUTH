c5ac9a32eb656501dab2b17cc8bbcaf6b6b68e27
gmlorenz <35004437+gmlorenz@users.noreply.github.com>
2020-03-31 14:44:38 +0800

    Update serverinstaller.sh

--- serverinstaller.sh
+++ serverinstaller.sh
@@ -1,255 +1,94 @@
 #!/bin/bash
 ##
 ## Debian VPN Server Installer Script
-## by Lorenz Ebrado
+## by Renz
 ##
 ## Copyright (c) Debian VPN 2019. All Rights Reserved
 ##
 
-## Prepare System
-##
-# Disable ipv6
-echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
-# Set Local Time
-ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime
 
-##
-## Update and Upgrade System
-##
-apt update 
-apt upgrade -y
+## Check if VPS have root access
+if [[ "$EUID" -ne 0 ]]; then
+	echo "Sorry, you need to run this as root"
+	exit
+fi
 
-##
-## Install Applications
-##
-apt install openvpn stunnel4 privoxy squid3 dropbear ufw build-essential fail2ban libpam-mysql unzip -y
+## Check if TUN Device is available
+if [[ ! -e /dev/net/tun ]]; then
+	echo "The TUN device is not available
+You need to enable TUN before running this script"
+	exit
+fi
 
-##
-## Configure DropBear
-##
-sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=442/g' /etc/default/dropbear
-echo "/bin/false" >> /etc/shells
+## Check if VPS have Debian OS
+if [[ -e /etc/debian_version ]]; then
+	OS=debian
+	GROUPNAME=nogroup
+	RCLOCAL='/etc/rc.local'
+else
+	echo "Looks like you aren't running this installer on Debian, Ubuntu or CentOS"
+	exit
+fi
 
-##
-## Configure Privoxy
-##
-cat > /etc/privoxy/config <<-END
-user-manual /usr/share/doc/privoxy/user-manual
-confdir /etc/privoxy
-logdir /var/log/privoxy
-filterfile default.filter
-logfile logfile
-listen-address  0.0.0.0:3356
-listen-address  0.0.0.0:8086
-toggle  1
-enable-remote-toggle  0
-enable-remote-http-toggle  0
-enable-edit-actions 0
-enforce-blocks 0
-buffer-limit 4096
-enable-proxy-authentication-forwarding 1
-forwarded-connect-retries  1
-accept-intercepted-requests 1
-allow-cgi-request-crunching 1
-split-large-forms 0
-keep-alive-timeout 5
-tolerate-pipelining 1
-socket-timeout 300
-permit-access 0.0.0.0/0 xxxxxxxxx
-END
-sed -i $MYIP2 /etc/privoxy/config;
+## GET REAL IP
+if [[ "$PUBLICIP" != "" ]]; then
+	IP=$PUBLICIP
+fi
 
-##
+## Updating System and Installing OpenVPN and other Application
+apt-get update
+apt-get install openvpn squid ufw mysql-client unzip -y
+
+## Packet Forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i '/net.ipv4.ip_forward=1/s/^#//g' /etc/sysctl.conf
+ufw allow ssh
+ufw allow 443/tcp
+ufw allow 8000/tcp
+ufw allow 8080/tcp
+sed -i 's/\(DEFAULT_FORWARD_POLICY=\).*/\1"ACCEPT"/' /etc/default/ufw
+sed -i '11i# START OPENVPN RULES\n# NAT table rules\n*nat\n:POSTROUTING ACCEPT [0:0]\n# Allow traffic from OpenVPN client to eth0\n-A POSTROUTING -s 10.8.0.0/8 -o eth0 -j MASQUERADE\nCOMMIT\n# END OPENVPN RULES\n' /etc/ufw/before.rules
+echo y | ufw enable
+
 ## Configure Squid Proxy
-##
-cat > /etc/squid/squid.conf <<-END
-acl localhost src 127.0.0.1/32 ::1
-acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
-acl localnet src 10.0.0.0/8
-acl localnet src 172.16.0.0/12
-acl localnet src 192.168.0.0/16
-acl localnet src fc00::/7
-acl localnet src fe80::/10
-acl SSL_ports port 80-8085
-acl Safe_ports port 80
-acl Safe_ports port 21
-acl Safe_ports port 443
-acl Safe_ports port 444
-acl Safe_ports port 70
-acl Safe_ports port 210
-acl Safe_ports port 1025-65535
-acl Safe_ports port 280
-acl Safe_ports port 488
-acl Safe_ports port 591
-acl Safe_ports port 777
-acl CONNECT method CONNECT
-acl SSH dst xxxxxxxxx-xxxxxxxxx/32
-acl SSH dst 103.103.0.118-103.103.0.118/32
-http_access allow SSH
-http_access allow localnet
-http_access allow manager localhost
-http_access deny manager
-http_access allow localhost
-http_access deny all
-http_port 8085
-http_port 3355
-coredump_dir /var/spool/squid3
-refresh_pattern ^ftp: 1440 20% 10080
-refresh_pattern ^gopher: 1440 0% 1440
-refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
-refresh_pattern . 0 20% 4320
-visible_hostname JSystem
-END
-sed -i $MYIP2 /etc/squid/squid.conf;
-
-##
-## Configure SSH Banner
-##
-rm /etc/issue.net
-echo $banner > /etc/issue.net
-sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
-sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
-service ssh restart
-service dropbear restart
+wget -O /etc/squid3/squid.conf https://raw.githubusercontent.com/johndesu090/OVPNAUTH/master/squid.conf
+IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+sed -i "s/ipmokasito/$IP/g" /etc/squid3/squid.conf
 
 ## Download OpenVPN Files
 cd /etc/openvpn/
 rm *
-https://www.dropbox.com/s/wagkvyj0fl40vg4/openvpn.zip?dl=1
+wget https://raw.githubusercontent.com/johndesu090/OVPNAUTH/master/openvpn.zip
 unzip openvpn.zip
 rm openvpn.zip
 
-##
-## Configure STunnel
-##
-sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -sha256 -subj '/CN=127.0.0.1/O=localhost/C=PH' -keyout /etc/stunnel/stunnel.pem -out /etc/stunnel/stunnel.pem
-cat > /etc/stunnel/stunnel.conf <<-END
+## Download User Count Script
+cd ~
+wget https://raw.githubusercontent.com/johndesu090/OVPNAUTH/master/users.sh
+crontab -l > mycron
+echo "* * * * * ~/users.sh" >> mycron
+crontab mycron
+rm mycron
 
-sslVersion = all
-pid = /stunnel.pid
-socket = l:TCP_NODELAY=1
-socket = r:TCP_NODELAY=1
-client = no
+## Setting Permission
+chmod +x ~/users.sh
+chmod +x /etc/openvpn/login.sh
 
-[openvpn]
-accept = 587
-connect = 127.0.0.1:1147
-cert = /etc/stunnel/stunnel.pem
-
-[dropbear]
-accept = 444
-connect = 127.0.0.1:442
-cert = /etc/stunnel/stunnel.pem
-
-END
-
-##
-## Setup Firewall
-##
-ufw allow ssh
-ufw allow 1194/tcp
-sed -i 's|DEFAULT_INPUT_POLICY="DROP"|DEFAULT_INPUT_POLICY="ACCEPT"|' /etc/default/ufw
-sed -i 's|DEFAULT_FORWARD_POLICY="DROP"|DEFAULT_FORWARD_POLICY="ACCEPT"|' /etc/default/ufw
-
-##
-## Set ipv4 Forward
-##
-echo 1 > /proc/sys/net/ipv4/ip_forward
-sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
-sysctl -p
-
-
-##
-## Setup IPtables
-##
-cat > /etc/iptables.up.rules <<-END
-*nat
-:PREROUTING ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
-:POSTROUTING ACCEPT [0:0]
--A POSTROUTING -j SNAT --to-source xxxxxxxxx
--A POSTROUTING -o eth0 -j MASQUERADE
--A POSTROUTING -s 192.168.10.0/24 -o eth0 -j MASQUERADE
-COMMIT
-*filter
-:INPUT ACCEPT [0:0]
-:FORWARD ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
-:fail2ban-ssh - [0:0]
--A INPUT -p tcp -m multiport --dports 22 -j fail2ban-ssh
--A INPUT -p ICMP --icmp-type 8 -j ACCEPT
--A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
--A INPUT -p tcp --dport 22  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 80  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 143  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 442  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 443  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 444  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 587  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 1147  -m state --state NEW -j ACCEPT
--A INPUT -p udp --dport 1147  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 3355  -m state --state NEW -j ACCEPT
--A INPUT -p udp --dport 3355  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 8085  -m state --state NEW -j ACCEPT
--A INPUT -p udp --dport 8085  -m state --state NEW -j ACCEPT 
--A INPUT -p tcp --dport 3356  -m state --state NEW -j ACCEPT
--A INPUT -p udp --dport 3356  -m state --state NEW -j ACCEPT
--A INPUT -p tcp --dport 8086  -m state --state NEW -j ACCEPT
--A INPUT -p udp --dport 8086  -m state --state NEW -j ACCEPT 
--A INPUT -p tcp --dport 10000  -m state --state NEW -j ACCEPT
--A fail2ban-ssh -j RETURN
-COMMIT
-*raw
-:PREROUTING ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
-COMMIT
-*mangle
-:PREROUTING ACCEPT [0:0]
-:INPUT ACCEPT [0:0]
-:FORWARD ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
-:POSTROUTING ACCEPT [0:0]
-COMMIT
-END
-sed -i $MYIP2 /etc/iptables.up.rules;
-iptables-restore < /etc/iptables.up.rules
-
-##
-## Configure DNS
-##
-# Create and Configure rc.local
-cat > /etc/rc.local <<-END
-#!/bin/sh -e
-
-exit 0
-END
-chmod +x /etc/rc.local
-sed -i '$ i\echo "nameserver 1.1.1.1" > /etc/resolv.conf' /etc/rc.local
-sed -i '$ i\echo "nameserver 1.0.0.1" >> /etc/resolv.conf' /etc/rc.local
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-
-##
-## Restart Services
-##
+## Start Debian VPN
 service openvpn restart
-service dropbear restart
-service fail2ban restart
-service squid restart
-service privoxy restart
-
-## 
-## Clear History
-##
-history -c
-rm -rf /root/*
-cd /root
-
-##
-## Information
-##
+service squid3 restart
 clear
-echo " "
-echo "Status: Done"
-echo " "
+ovpn=openvpn
+proxy=squid3
+if (( $(ps -ef | grep -v grep | grep $ovpn | wc -l) > 0 ))
+	then
+	echo "OpenVPN is Running"
+	else
+	echo "Error Starting OpenVPN. Please contact JohnFordTV"
+fi
+if (( $(ps -ef | grep -v grep | grep $proxy | wc -l) > 0 ))
+	then
+	echo "Squid Proxy is Running"
+	else
+	echo "Error Starting Squid Proxy. Please contact JohnFordTV"
+fi 
